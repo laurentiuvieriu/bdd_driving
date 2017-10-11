e8b0a26877e317fe4da8b4f57be2d9e14341d281
Laurentiu Vieriu <laurentiuvieriu@gmail.com>
2017-10-11 14:26:51 +0200

    finished visualization tool_1 for kitti

--- rv_plot_results_kitti.py
+++ rv_plot_results_kitti.py
@@ -16,18 +16,103 @@ targetDir = '/media/radu/sdb_data/radu/work/python/bdd_driving/results/kitti'
 date = '2011_09_30'
 drive = '0028'
 dataset = pykitti.raw(basedir, date, drive, imformat='cv2')
-cvfont = cv2.FONT_HERSHEY_SIMPLEX
-cvfont_size = 0.5
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_size = 0.5
+font_thickness = 1
+rect_width = 5
+rect_max = 30 # maximum length of a line in pixels
+rect_center_x = 240
+model_input_w = 640
+model_input_h = 360
+
 # cvfont_thickness = 1
 # batch_size = 32
 # win_size = 20
 # IMSZ = 228
 
-fid = open(targetDir + "/" + date + "_drive_" + drive + "_sync.csv", "rb")
+noFrames = dataset.__len__()
+
+fid = open(targetDir + "/" + date + "_drive_" + drive + "_sync_full.csv", "rb")
 reader = csv.reader(fid, delimiter=',')
+# row_count = np.sum(1 for row in reader)
 csv_header = next(reader)
 
-line = next(reader)
+all_labs = []
+for k in range(noFrames):
+    line = next(reader)
+    all_labs.append(np.asarray([float(item) for item in line]))
+
+all_labs = np.stack(all_labs)
+fid.close()
+lab_max = all_labs.max(0)
+lab_min = all_labs.min(0)
+max_stretch = []
+max_stretch.append(np.abs(lab_max))
+max_stretch.append(np.abs(lab_min))
+max_stretch = np.stack(max_stretch)
+lab_range = max_stretch.max(0)
+
+iter_cam2 = iter(dataset.cam2)
+
+def plot_bar_on_img(img, mag, x_center, y_pos, bar_width, label, font, font_size, font_thickness):
+    cv2.putText(img_crop, label, (10, y_pos), font, font_size, (0,255,0), font_thickness, cv2.LINE_AA)
+    if mag < 0:
+        cv2.rectangle(img, (x_center+ mag, y_pos- bar_width), (x_center, y_pos), (0,0,255), bar_width)
+    else:
+        cv2.rectangle(img, (x_center, y_pos- bar_width), (x_center+ mag, y_pos), (0,255,0), bar_width)
+    return img
+
+# assume the results and the img files are synchronized
+# csv_header = ('img_idx', 'vf (m/s)', 'af (m/s^2)', 'wz (deg/s)', 'res0', 'res1', 'res2', 'res3', 'res4', 'res5')
+# action_map = {-1:'not_sure', 0:'straight', 1:'slow_or_stop',
+#                         2:'turn_left', 3:'turn_right',
+#                         4:'turn_left_slight', 5:'turn_right_slight'}
+
+for k in range(noFrames):
+    row_pos = iter(range(20, 360, 20))
+    img = np.array(next(iter_cam2))
+    img_crop = rv_imgCropCenter(img, model_input_w, model_input_h)
+    local_res = all_labs[k]
+    labels = [float(item) for item in local_res]
+    cv2.putText(img_crop, "velocity fwd (vf)...................... {:02.2f}".format(local_res[1]), (10,next(row_pos)), font, font_size, (0,255,0), font_thickness, cv2.LINE_AA)
+#     cv2.putText(img_crop, "acceleration fwd (af).................. {:02.2f}".format(local_res[1]), (10,next(row_pos)), font, font_size, (0,0,255), font_thickness, cv2.LINE_AA)
+#     cv2.putText(img_crop, "angular rate around z (wz)............. {:02.2f}".format((local_res[2])*180/np.pi), (10,next(row_pos)), font, font_size, (0,0,255), font_thickness, cv2.LINE_AA)
+    # deal with acceleration
+    accel_mag_gt = int(np.round(local_res[2]/lab_range[2]*rect_max))
+    plot_bar_on_img(img_crop, accel_mag_gt, rect_center_x, next(row_pos), rect_width, 'accel_gt:', font, font_size, font_thickness)
+    
+    row_pos_local = next(row_pos)
+    fwd_pred = int(np.round(local_res[4]/lab_range[4]*rect_max))
+    plot_bar_on_img(img_crop, fwd_pred, rect_center_x, row_pos_local, rect_width, 'fwd/slow:', font, font_size, font_thickness)
+
+    slow_pred = -int(np.round(local_res[5]/lab_range[5]*rect_max))
+    plot_bar_on_img(img_crop, slow_pred, rect_center_x, row_pos_local, rect_width, 'fwd/slow:', font, font_size, font_thickness)
+    
+    # deal with steering
+    lr_mag_gt = -int(np.round(local_res[3]/lab_range[3]*rect_max))
+    plot_bar_on_img(img_crop, lr_mag_gt, rect_center_x, next(row_pos), rect_width, 'left/right_gt:', font, font_size, font_thickness)
+    
+    row_pos_local = next(row_pos)
+    left_pred = -int(np.round(local_res[6]/lab_range[6]*rect_max))
+    plot_bar_on_img(img_crop, left_pred, rect_center_x, row_pos_local, rect_width, 'left/right_pred:', font, font_size, font_thickness)
+
+    right_pred = int(np.round(local_res[7]/lab_range[7]*rect_max))
+    plot_bar_on_img(img_crop, right_pred, rect_center_x, row_pos_local, rect_width, 'left/right_pred:', font, font_size, font_thickness)
+    
+    # deal with slight_steering    
+    row_pos_local = next(row_pos)
+    slight_left_pred = -int(np.round(local_res[8]/lab_range[8]*rect_max))
+    plot_bar_on_img(img_crop, slight_left_pred, rect_center_x, row_pos_local, rect_width, 'left/right_pred:', font, font_size, font_thickness)
+
+    slight_right_pred = int(np.round(local_res[9]/lab_range[8]*rect_max))
+    plot_bar_on_img(img_crop, slight_right_pred, rect_center_x, row_pos_local, rect_width, 'shight left/right_pred:', font, font_size, font_thickness)
+    
+    cv2.imshow('image', img_crop)
+    cv2.moveWindow('image', 2500, 360)
+    cv2.waitKey(30)
+#     cv2.imwrite(targetDir+"/" + date + "_drive_" + drive + "_sync_full/frame_{:010d}.png".format(k), img_crop)
+
+# line = next(reader)
 
 
 
